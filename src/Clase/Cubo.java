f75bb3318b30bedd205fce1bae8326ef81af03e1
jose andrango <joseandrango64@gmail.com>
2021-06-07 18:48:50 -0500

    Deber-3725-Jose.Andrango

--- src/Clase/Cubo.java
+++ src/Clase/Cubo.java
@@ -6,9 +6,17 @@ public class Cubo {
     int alto;
     int profundo;
     //construtor
-
-    public Cubo(int ancho) {
+    public Cubo(int ancho, int alto, int profundo) {
         this.ancho = ancho;
+        this.alto = alto;
+        this.profundo = profundo;
     }
-    
+
+    public Cubo() {
+        System.out.println("Deber del Cubo");
+    }
+    public int CalculoCubo(){
+        int cubo1 = (ancho * alto * profundo) ;
+        return cubo1;
+    }
 }
