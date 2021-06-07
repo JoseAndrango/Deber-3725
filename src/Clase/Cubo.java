2723bdf2c7a5d2620d22adc08e318aa80ea1469b
jose andrango <joseandrango64@gmail.com>
2021-06-07 18:10:24 -0500

    Deber-3725-Jose.Andrango

---
+++ src/Clase/Cubo.java
@@ -0,0 +1,14 @@
+
+package Clase;
+
+public class Cubo {
+    int ancho;
+    int alto;
+    int profundo;
+    //construtor
+
+    public Cubo(int ancho) {
+        this.ancho = ancho;
+    }
+    
+}
